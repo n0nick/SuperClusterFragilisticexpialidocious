0e6e0079673ecc6c0712cbbb1c31c909a34d1d29
Sagie Maoz <sagie@maoz.info>
2012-05-21 14:39:55 +0300

    adding main input loop.
    - handling EOF.
    - using fgets() instead of scanf().
    - handling 'quit' action.

--- main.c
+++ main.c
@@ -8,25 +8,36 @@
 
 int main(int argc, char* argv[])
 {
-	// vars
 	char* input;
 	struct command cmd;
 	int i = 0;
+	int quit = 0;
 
 	input = (char *) malloc(MAX_INPUT_SIZE);
 
-	// Reading input
-	scanf("%[^\n\r]s", input);
-	cmd = parse(input);
+	while (!quit) {
+		printf("Give me some input: ");
+		if (fgets(input, MAX_INPUT_SIZE, stdin) != NULL) {
+			cmd = parse(input);
 
-	// Looping until getting quit
-	printf("command was: %s\n\r", cmd.action);
-	while(i < cmd.arguments_count) {
-		printf("arguments %d is: %s\n\r", i+1, cmd.arguments[i]);
-		i++;
+			if (strcmp(cmd.action, COMMAND_QUIT) == 0) {
+				quit = 1;
+				printf("I quit!\n");
+			} else {
+				printf("Command was: %s\n", cmd.action);
+				i = 0;
+				while(i < cmd.arguments_count) {
+					printf("Arguments %d was: %s\n", i+1, cmd.arguments[i]);
+					i++;
+				}
+			}
+		} else { // EOF received
+			quit = 1;
+		}
 	}
 
 	// Exiting
+	printf("\nThank you, goodbye.\n");
 	return 0;
 }
 
